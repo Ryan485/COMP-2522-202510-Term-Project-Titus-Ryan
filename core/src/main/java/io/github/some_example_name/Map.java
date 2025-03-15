3fc08a8d7b8e3b943be5736c45926cc41132addf
Titus <ttlleegml@gmail.com>
2025-03-14 17:21:03 -0700

    Create skeleton for map

---
+++ core/src/main/java/io/github/some_example_name/Map.java
@@ -0,0 +1,16 @@
+package io.github.some_example_name;
+
+public class Map {
+    String biome;
+    int wind;
+
+    public Map(String biome) {
+        this.biome = biome;
+    }
+
+    public void setWind(int wind) {}
+    public int getWind() {return 0;}
+    public void getBiome() {}
+    public void setBackgroundImage(String backgroundImage) {}
+    public void render(){}
+}
