f117517b0d47397885da63a19e5206603a9b3d46
Ryan485 <song976431@gmail.com>
2025-03-14 17:21:28 -0700

    TankPlayer in progress

---
+++ core/src/main/java/io/github/some_example_name/TankPlayer.java
@@ -0,0 +1,21 @@
+package io.github.some_example_name;
+
+public class TankPlayer {
+    String colour;
+    int speed;
+    int money;
+    int fuel;
+    int HP;
+
+
+    public TankPlayer(String colour, int speed, int money, int fuel, int HP) {
+        this.colour = colour;
+        this.speed = speed;
+        this.money = money;
+        this.fuel = fuel;
+        this.HP = HP;
+    }
+
+
+
+}
