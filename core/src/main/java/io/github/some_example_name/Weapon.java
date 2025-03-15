5fedb426a34f121062543d8d52316adfa3f6784d
Titus <ttlleegml@gmail.com>
2025-03-14 18:26:41 -0700

    fixed weapon abstract functions and instance variables

--- core/src/main/java/io/github/some_example_name/Weapon.java
+++ core/src/main/java/io/github/some_example_name/Weapon.java
@@ -1,13 +1,12 @@
 package io.github.some_example_name;
 
 public abstract class Weapon {
-    int speed;
-    int damage;
+    protected int speed, damage;
 
     public Weapon(int speed, int damage) {
         this.speed = speed;
         this.damage = damage;
     }
-    abstract public int setDamage();
-    abstract public void setSpeed();
+    abstract public void setDamage(int damage);
+    abstract public void setSpeed(int speed);
 }
