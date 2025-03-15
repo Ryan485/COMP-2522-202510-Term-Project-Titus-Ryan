41376714dc8225976f4683a5c2719f64a8875d67
Ryan485 <song976431@gmail.com>
2025-03-14 17:51:41 -0700

    Create Weapon class

---
+++ core/src/main/java/io/github/some_example_name/Weapon.java
@@ -0,0 +1,23 @@
+package io.github.some_example_name;
+
+public abstract class Weapon {
+    int speed;
+    String colour;
+
+    public Weapon(int speed, String colour) {
+        this.speed = speed;
+        this.colour = colour;
+    }
+
+    private void nuclear_bomb() {
+
+    }
+
+    private void volcano_bomb() {
+
+    }
+
+    private void bouncing_bomb() {
+
+    }
+}
