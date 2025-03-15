9384615068add2403c5d6fd7ab72b202363dcdcb
Titus <ttlleegml@gmail.com>
2025-03-14 18:27:34 -0700

    fixed shop inventory

--- core/src/main/java/io/github/some_example_name/Shop.java
+++ core/src/main/java/io/github/some_example_name/Shop.java
@@ -3,13 +3,17 @@ package io.github.some_example_name;
 import java.util.HashMap;
 
 public class Shop {
-    HashMap<String, Integer> Inventory;
+    //<name,[Price,Qty]>
+    HashMap<String, Integer[]> Inventory;
     String name;
     public Shop() {
         Inventory = new HashMap<>();
         //fill in the items and price
     }
+    private boolean isAvailable(final String item) {
+        return Inventory.get(item)[1] > 0;
+    }
     public int getPrice(final String item) {
-        return Inventory.get(item);
+        return Inventory.get(item)[0];
     }
 }
