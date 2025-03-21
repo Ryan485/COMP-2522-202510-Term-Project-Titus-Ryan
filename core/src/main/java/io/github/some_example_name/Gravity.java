0adcd28a2232328b731ae2fcb7fcfc2d463451bc
Titus <ttlleegml@gmail.com>
2025-03-21 12:15:47 -0700

    added gravity interface

---
+++ core/src/main/java/io/github/some_example_name/Gravity.java
@@ -0,0 +1,13 @@
+package io.github.some_example_name;
+
+/**
+ * Provide downward gravity to objects.
+ * @author Titus
+ * @version 2015
+ */
+public interface Gravity {
+    /**
+     * to activate gravity.
+     */
+    void applyGravity();
+}
