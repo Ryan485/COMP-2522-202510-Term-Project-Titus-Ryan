db97cb7e410d69356fa065a8b545aa73a263f5f5
Ryan485 <song976431@gmail.com>
2025-03-21 18:49:19 -0700

    Initialize instance variables on the constructor and create setters

--- core/src/main/java/io/github/some_example_name/NuclearBomb.java
+++ core/src/main/java/io/github/some_example_name/NuclearBomb.java
@@ -8,17 +8,36 @@ public class NuclearBomb extends Bomb{
     private int damageRadius;
     private int radius;
     private Texture nuclearBombtexture;
-    public NuclearBomb(final int damageRadius, final int radius, final int speed, final int damage) {
+    private float x,y;   // float coordinates for smooth motion
+    private float velocity_x, velocity_y;    // velocity components
+
+
+
+    public NuclearBomb(final int damageRadius, final int radius, final int speed,
+                       final int damage, float x, float y, int angle) {
         super(speed, damage);
         this.damageRadius = damageRadius;
         this.radius = radius;
+        this.nuclearBombtexture = new Texture(Gdx.files.internal("assets/bomb.png"));
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.velocity_x = (float) Math.cos(Math.toRadians(angle)) * speed;
+        this.velocity_y = (float) Math.sin(Math.toRadians(angle)) * speed;
+
     }
     public void setDamageRadius(final int damageRadius) {
         this.damageRadius = damageRadius;
     }
+
+    @Override
     public void setSpeed(final int speed) {
         this.speed = speed;
+        this.velocity_x = (float) Math.cos(Math.toRadians(angle)) * speed;
+        this.velocity_y = (float) Math.sin(Math.toRadians(angle)) * speed;
     }
+
+    @Override
     public void setDamage(final int damage) {
         this.damage = damage;
     }
